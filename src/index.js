61729e2e03a89b3d5a0ff40e2d1a420a14cf3db4
Nathanstuff <n.odonnell@hotmail.com>
2020-12-26 13:33:10 +1100

    bird sprite loads

--- src/index.js
+++ src/index.js
@@ -14,12 +14,18 @@ const config = {
   }
 }
 
+// sets the variables
 function preload() {
   this.load.image('sky', 'assets/sky.png')
+  this.load.image('bird', 'assets/bird.png')
 }
+
+let bird
 
 function create() {
-  this.add.image(config.width / 2, config.height / 2, 'sky')
-}
+  this.add.image(config.width / 2, config.height / 2, 'sky');
+  // alternatively, image(0,0,'sky').setOrigin(0,0)
 
+  bird = this.add.sprite(config.width/10, config.height/2, 'bird').setOrigin(0);
+}
 new Phaser.Game(config)
